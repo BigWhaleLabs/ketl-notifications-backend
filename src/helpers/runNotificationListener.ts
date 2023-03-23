88970025e700413bf7006f7928a123b04a5b1dcd
avr6ude <canuckchan@gmail.com>
2023-03-23 12:41:24 -0500

    Add environment check to feeds

--- src/helpers/runNotificationListener.ts
+++ src/helpers/runNotificationListener.ts
@@ -1,14 +1,22 @@
 import { TokenModel } from '@/models/Token'
+import env from '@/helpers/env'
 import obssContract from '@/helpers/getObssContract'
 import sendAppleNotification from '@/helpers/sendAppleNotification'
 import sendGoogleNotification from '@/helpers/sendGoogleNotification'
 
-const apnRegex = /^[a-f0-9]{64}$/
-
-const rootFeeds = {
+const prodFeeds = {
   1: 't/startups',
   2: 't/ketlTeam',
-} as { [key: number]: string }
+}
+
+const apnRegex = /^[a-f0-9]{64}$/
+
+const rootFeeds: { [key: number]: string } = env.isProduction
+  ? prodFeeds
+  : {
+      0: 't/devFeed',
+      ...prodFeeds,
+    }
 
 obssContract.on('FeedPostAdded', async (feedId) => {
   const title = rootFeeds[feedId.toNumber()]
