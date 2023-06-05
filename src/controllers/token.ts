bf873bfb9201a0317771525024b181149937c32f
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-06-05 17:32:44 +0400

    remove notifications from token controller

--- src/controllers/token.ts
+++ src/controllers/token.ts
@@ -1,46 +1,11 @@
-import { Body, Controller, Ctx, Delete, Get, Post, Query } from 'amala'
+import { Body, Controller, Ctx, Delete, Post, Query } from 'amala'
 import { Context } from 'koa'
 import { TokenModel } from '@/models/Token'
 import { badRequest } from '@hapi/boom'
-import {
-  proccessCommentsForNotifications,
-  proccessPostsForNotifications,
-} from '@/helpers/proccessBlocksForNotifications'
 import Token from '@/validators/Token'
-import defaultProvider from '@/helpers/defaultProvider'
-import getEvents from '@/helpers/getEventst'
-import getFeedsContract from '@/helpers/getFeedsContract'
 
 @Controller('/token')
 export default class TokenController {
-  @Get('/')
-  async getData(@Query('currentBlockNumber') currentBlockNumber?: number) {
-    const currentBlock = await defaultProvider.getBlockNumber()
-    const commentsSinceLastCheck = await getEvents(
-      getFeedsContract.filters.CommentAdded(),
-      currentBlockNumber ? currentBlockNumber + 1 : currentBlock,
-      currentBlock
-    )
-    const postsSinceLastCheck = await getEvents(
-      getFeedsContract.filters.PostAdded(),
-      currentBlockNumber ? currentBlockNumber + 1 : currentBlock,
-      currentBlock
-    )
-
-    const modifiedPostsSinceLastCheck =
-      proccessPostsForNotifications(postsSinceLastCheck)
-
-    const modifiedCommentSinceLastCheck = proccessCommentsForNotifications(
-      commentsSinceLastCheck
-    )
-
-    return {
-      currentBlock,
-      modifiedCommentSinceLastCheck,
-      modifiedPostsSinceLastCheck,
-    }
-  }
-
   @Post('/')
   async addToken(
     @Body({ required: true }) { token }: Token,
