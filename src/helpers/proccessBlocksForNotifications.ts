ba4702542b5da26702cc4921041de00b3ea82fa0
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-06-19 15:07:40 +0400

    extract function

--- src/helpers/proccessBlocksForNotifications.ts
+++ src/helpers/proccessBlocksForNotifications.ts
@@ -2,51 +2,15 @@ import {
   CommentAddedEvent,
   PostAddedEvent,
 } from '@big-whale-labs/obss-storage-contract/dist/typechain/contracts/Feeds'
-import generateRandomName from '@/helpers/generateRandomName'
-import structToCid from '@/helpers/structToCid'
+import { parseComemnt } from '@/helpers/parse'
+import { parsePost } from '@/helpers/parse'
 
 export function proccessCommentsForNotifications(
   comments: CommentAddedEvent[]
 ) {
-  return comments.map(({ args }) => {
-    const feedId = args[0].toNumber()
-    const postId = args[1].toNumber()
-    const commentId = args[2].toNumber()
-    const comment = args[3]
-    const [sender, metadata, timestamp, threadId, replyTo, numberOfComments] =
-      comment
-    return {
-      commentId,
-      feedId,
-      metadata: structToCid(metadata),
-      numberOfComments: numberOfComments.toNumber(),
-      postId,
-      replyTo: replyTo.toNumber(),
-      sender,
-      threadId: threadId.toNumber(),
-      timestamp: timestamp.toNumber(),
-      username: generateRandomName(sender),
-    }
-  })
+  return comments.map(parseComemnt)
 }
 
 export function proccessPostsForNotifications(posts: PostAddedEvent[]) {
-  return posts.map(({ args }) => {
-    const feedId = args[0].toNumber()
-    const postId = args[1].toNumber()
-    const post = args[2]
-    const [sender, metadata, timestamp, threadId, replyTo, numberOfComments] =
-      post
-    return {
-      feedId,
-      metadata: structToCid(metadata),
-      numberOfComments: numberOfComments.toNumber(),
-      postId,
-      replyTo: replyTo.toNumber(),
-      sender,
-      threadId: threadId.toNumber(),
-      timestamp: timestamp.toNumber(),
-      username: generateRandomName(sender),
-    }
-  })
+  return posts.map(parsePost)
 }
