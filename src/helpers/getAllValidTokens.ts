c420c7235d956eba7e916ad145c4f27de6c437cb
Michel Erokhov <mixaileroxov@gmail.com>
2023-07-11 21:41:21 +0200

    add getAllValidTokens

---
+++ src/helpers/getAllValidTokens.ts
@@ -0,0 +1,8 @@
+import { TokenModel } from '@/models/Token'
+
+const apnRegex = /^[a-f0-9]{64}$/
+
+export default async function () {
+  const tokens = await TokenModel.distinct('token')
+  return tokens.filter((token) => apnRegex.test(token))
+}
