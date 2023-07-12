f24b74729434dd1a7f91b13aae41c01a339bb3a4
Michel Erokhov <mixaileroxov@gmail.com>
2023-07-12 17:45:07 +0200

    fix regex filter

--- src/helpers/getAllValidTokens.ts
+++ src/helpers/getAllValidTokens.ts
@@ -4,5 +4,5 @@ const apnRegex = /^[a-f0-9]{64}$/
 
 export default async function () {
   const tokens = await TokenModel.distinct('token')
-  return tokens.filter(apnRegex.test)
+  return tokens.filter((token) => apnRegex.test(token))
 }
