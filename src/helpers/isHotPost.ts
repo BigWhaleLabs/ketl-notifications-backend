c0617cbbe5ee092829041de3b96e2bdda9cde0d2
Michel Erokhov <mixaileroxov@gmail.com>
2023-09-23 01:37:56 +0200

    fix isHotPost

--- src/helpers/isHotPost.ts
+++ src/helpers/isHotPost.ts
@@ -2,17 +2,15 @@ import axios from 'axios'
 import env from '@/helpers/env'
 import getFeedsContract from '@/helpers/getFeedsContract'
 
-async function getParticipantsAndNumberOfComments(
-  feedId: number,
-  postId: number
-) {
-  const [{ participants, post }] =
-    await getFeedsContract.getPostsAndParticipants(feedId, postId, 1, false)
+async function getParticipants(feedId: number, postId: number) {
+  const [{ participants }] = await getFeedsContract.getPostsAndParticipants(
+    feedId,
+    postId,
+    1,
+    false
+  )
 
-  return {
-    numberOfComments: post.numberOfComments.toNumber(),
-    participants: participants.length,
-  }
+  return participants.length
 }
 
 async function getNumberOfComments(feedId: number, postId: number) {
@@ -24,6 +22,12 @@ async function getNumberOfComments(feedId: number, postId: number) {
 async function getNumberOfLikes(feedId: number, postId: number) {
   const [_, likes] = await getFeedsContract.getReactions(feedId, postId, 0)
   return likes.toNumber()
+}
+
+const week = 1000 * 60 * 60 * 24 * 7
+async function isOldPost(feedId: number, postId: number) {
+  const post = await getFeedsContract.posts(feedId, postId)
+  return Date.now() - post.timestamp.toNumber() * 1000 > week
 }
 
 const viewsEndpoint = `${env.KETL_VIEWS_BACKEND}/views`
@@ -42,13 +46,20 @@ export async function getViews(feedId: number, postId: number) {
 }
 
 export default async function isHotPost(feedId: number, postId: number) {
-  const { participants } = await getParticipantsAndNumberOfComments(
-    feedId,
-    postId
-  )
+  const isOld = await isOldPost(feedId, postId)
+  if (isOld) return false
+
+  const participants = await getParticipants(feedId, postId)
+
+  if (participants < 3) return false
+
   const numberOfComments = await getNumberOfComments(feedId, postId)
+  if (numberOfComments < 7) return false
+
   const likes = await getNumberOfLikes(feedId, postId)
+  if (likes < 10) return false
+
   const views = await getViews(feedId, postId)
 
-  return views >= 25 && numberOfComments > 7 && likes >= 10 && participants >= 3
+  return views >= 25
 }
