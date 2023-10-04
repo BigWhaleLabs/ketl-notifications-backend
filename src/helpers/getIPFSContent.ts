13b833c167e7c620b9166ce6ea0f9d3137ee2847
Michel Erokhov <mixaileroxov@gmail.com>
2023-10-04 13:06:17 +0200

    refactor getIPFDContent

--- src/helpers/getIPFSContent.ts
+++ src/helpers/getIPFSContent.ts
@@ -5,8 +5,8 @@ export default async function (cid?: string) {
   if (!cid) return
   const ipfsBaseUrl = env.IPFS_GATEWAY
   const ipfsUrlString = `${ipfsBaseUrl}/${cid}`
-  const ipfsContent = (
-    await axios.get<{ text?: string; extraText?: string }>(ipfsUrlString)
-  ).data
-  return ipfsContent
+  const { data } = await axios.get<{ text?: string; extraText?: string }>(
+    ipfsUrlString
+  )
+  return data
 }
