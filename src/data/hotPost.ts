66313a218718aeb732d66990f26e556f3ef5e51c
Michel Erokhov <mixaileroxov@gmail.com>
2023-10-05 15:44:42 +0200

    set test values

--- src/data/hotPost.ts
+++ src/data/hotPost.ts
@@ -1,5 +1,5 @@
 export const oneWeek = 1000 * 60 * 60 * 24 * 7 // relevancePeriod
-export const minimumParticipants = 3
-export const minimumNumberOfComments = 7
-export const minimumLikes = 10
-export const minimumViews = 25
+export const minimumParticipants = 1
+export const minimumNumberOfComments = 2
+export const minimumLikes = 1
+export const minimumViews = 1
