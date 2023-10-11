4761a1436be1abd1ab6d162ade9be782403087cd
Michel Erokhov <mixaileroxov@gmail.com>
2023-10-12 00:34:28 +0200

    persist sended posts

--- src/helpers/runNotificationListener.ts
+++ src/helpers/runNotificationListener.ts
@@ -1,15 +1,12 @@
 import { BigNumber } from 'ethers'
 import { PostStructOutput } from '@big-whale-labs/obss-storage-contract/dist/typechain/contracts/Feeds'
-import { generateRandomName } from '@big-whale-labs/backend-utils'
 import { getTokens } from '@/models/Token'
 import { minimumNumberOfComments } from '@/data/hotPost'
 import checkAndSendHotPost from '@/helpers/sendHotPost'
-import feedsData from '@/helpers/feedsData'
 import getFeedsContract from '@/helpers/getFeedsContract'
-import getIPFSContent from '@/helpers/getIPFSContent'
 import ketlAttestationContract from '@/helpers/getKetlAttestation'
 import sendFirebaseNotification from '@/helpers/sendFirebaseNotification'
-import structToCid from '@/helpers/structToCid'
+import sendPost from '@/helpers/sendPost'
 
 ketlAttestationContract.on('EntanglementRegistered', async () => {
   try {
@@ -43,36 +40,12 @@ getFeedsContract.on(
 
 getFeedsContract.on(
   'PostAdded',
-  async (
-    feedId: BigNumber,
-    postId: BigNumber,
-    [author, metadata]: PostStructOutput
-  ) => {
+  async (feedId: BigNumber, postId: BigNumber, post: PostStructOutput) => {
     try {
       const numberFeedId = feedId.toNumber()
       const numberPostId = postId.toNumber()
 
-      const feedName = feedsData[numberFeedId]
-      if (!feedName) {
-        console.error('Feed not fount')
-        return
-      }
-      const content = await getIPFSContent(structToCid(metadata))
-      if (!content || !content.text) {
-        console.error('Post data is empty')
-        return
-      }
-
-      const authorUsername = generateRandomName(author)
-      const tokens = await getTokens({ allPostsEnabled: true })
-
-      await sendFirebaseNotification({
-        body: content.text,
-        feedId: numberFeedId,
-        postId: numberPostId,
-        title: `@${authorUsername} posted at ${feedName}`,
-        tokens,
-      })
+      await sendPost(numberFeedId, numberPostId, post)
     } catch (e) {
       console.error(e)
     }
---
+++ src/helpers/sendPost.ts
@@ -0,0 +1,45 @@
+import { PostStructOutput } from '@big-whale-labs/obss-storage-contract/dist/typechain/contracts/Feeds'
+import { generateRandomName } from '@big-whale-labs/backend-utils'
+import { getItem, setItem } from 'node-persist'
+import { getTokens } from '@/models/Token'
+import feedsData from '@/helpers/feedsData'
+import getIPFSContent from '@/helpers/getIPFSContent'
+import sendFirebaseNotification from '@/helpers/sendFirebaseNotification'
+import structToCid from '@/helpers/structToCid'
+
+async function isSendedHotPost(feedId: number, postId: number) {
+  const sendedDate = await getItem(`post-${feedId}-${postId}`)
+  return sendedDate !== undefined
+}
+
+export default async function sendPost(
+  feedId: number,
+  postId: number,
+  [author, metadata]: PostStructOutput
+) {
+  if (await isSendedHotPost(feedId, postId)) return
+
+  const feedName = feedsData[feedId]
+  if (!feedName) {
+    console.error('Feed not fount')
+    return
+  }
+  const content = await getIPFSContent(structToCid(metadata))
+  if (!content || !content.text) {
+    console.error('Post data is empty')
+    return
+  }
+
+  const authorUsername = generateRandomName(author)
+  const tokens = await getTokens({ allPostsEnabled: true })
+
+  await sendFirebaseNotification({
+    body: content.text,
+    feedId,
+    postId,
+    title: `@${authorUsername} posted at ${feedName}`,
+    tokens,
+  })
+
+  await setItem(`post-${feedId}-${postId}`, Date.now())
+}
